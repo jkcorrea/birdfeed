6fee2f1345e07b50fbdd97278238f85556f0cecf
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-08 16:25:21 -0800

    remove usecallback

--- app/lib/analytics/use-analytics.ts
+++ app/lib/analytics/use-analytics.ts
@@ -1,22 +1,18 @@
-import { useCallback } from 'react'
 import type { CaptureOptions, Properties } from 'posthog-js'
 import posthog from 'posthog-js'
 
 export function useAnalytics() {
-  const capture = useCallback(
-    (event_name: string, properties?: Properties | null | undefined, options?: CaptureOptions | undefined) =>
-      posthog.capture(event_name, properties, options),
-    []
-  )
+  const capture = (
+    event_name: string,
+    properties?: Properties | null | undefined,
+    options?: CaptureOptions | undefined
+  ) => posthog.capture(event_name, properties, options)
 
-  const identify = useCallback(
-    (
-      new_distinct_id: string | undefined,
-      userPropertiesToSet?: Properties | undefined,
-      userPropertiesToSetOnce?: Properties | undefined
-    ) => posthog.identify(new_distinct_id, userPropertiesToSet, userPropertiesToSetOnce),
-    []
-  )
+  const identify = (
+    new_distinct_id: string | undefined,
+    userPropertiesToSet?: Properties | undefined,
+    userPropertiesToSetOnce?: Properties | undefined
+  ) => posthog.identify(new_distinct_id, userPropertiesToSet, userPropertiesToSetOnce)
 
   return { capture, identify }
 }
