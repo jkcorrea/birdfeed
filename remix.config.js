b8758ebd0cd7bbe8e1f5aadd9f0ce3605476b79d
Jake Correa <jcorrea257@gmail.com>
2023-03-15 20:36:55 -0700

    bundle server deps...

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ const { flatRoutes } = require('remix-flat-routes')
  */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  serverDependenciesToBundle: [/^langchain*/, 'p-queue', 'p-timeout', 'gpt-3-encoder'],
   routes: (defineRoutes) => flatRoutes('routes', defineRoutes),
   future: {
     unstable_tailwind: true,
