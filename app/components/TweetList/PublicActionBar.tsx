652896e870b9e177a30c66ccaa8d17ec86376cf0
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-17 16:39:36 -0700

    Justin/design tweet card (#13)
    * redesigned cards
    
    * main cards

--- app/components/TweetList/PublicActionBar.tsx
+++ app/components/TweetList/PublicActionBar.tsx
@@ -12,24 +12,14 @@ export const PublicActionBar = ({ tweet }: { tweet: GeneratedTweet }) => {
   const hasSubmitted = Boolean(fetcher.state === 'submitting' || fetcher.data)
 
   return (
-    <div className="flex w-full items-center justify-end gap-4 pt-1 pb-2">
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href={buildSendTweetUrl(tweet.drafts[0], true)}
-        type="button"
-        className="btn-primary btn-ghost btn-sm btn"
-      >
-        Tweet it
-      </a>
-
+    <div className="flex w-full items-center justify-between gap-4 pt-1 pb-2">
       <fetcher.Form ref={zo.ref} action="/api/rate-tweet" method="post" className="inline-flex gap-3">
         <input readOnly name={zo.fields.tweetId()} type="hidden" value={tweet.id} />
         <button
           className={tw('text-neutral', hasSubmitted && 'cursor-default', fetcher.data?.upvoted && 'opacity-20')}
           disabled={hasSubmitted}
         >
-          <HandThumbDownIcon className="h-5 w-5" />
+          <HandThumbDownIcon className="h-6 w-6" />
           <span className="sr-only">Dislike</span>
         </button>
 
@@ -44,10 +34,20 @@ export const PublicActionBar = ({ tweet }: { tweet: GeneratedTweet }) => {
           name={zo.fields.upvote()}
           value="upvote"
         >
-          <HandThumbUpIcon className="h-5 w-5" />
+          <HandThumbUpIcon className="h-6 w-6" />
           <span className="sr-only">Like</span>
         </button>
       </fetcher.Form>
+
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={buildSendTweetUrl(tweet.drafts[0], true)}
+        type="button"
+        className="btn-primary btn-ghost btn-sm btn"
+      >
+        Tweet it
+      </a>
     </div>
   )
 }
