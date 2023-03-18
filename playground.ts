1a48114ef053768579eef328565b83721e9d926a
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-18 13:34:49 -0700

    increases chunk size, calls openai in parallel

---
+++ playground.ts
@@ -0,0 +1,6 @@
+// this will only work if you add  "type": "module" to your package.json
+
+// eslint-disable-next-line prettier/prettier
+(async () => {
+  console.log('Starting Playground...')
+})()
