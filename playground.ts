dde94b33a1a28a3951df2c11feb7ee215eb06ab3
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-18 19:21:56 -0700

    fixing issues

--- playground.ts
+++
@@ -1,6 +0,0 @@
-// this will only work if you add  "type": "module" to your package.json
-
-// eslint-disable-next-line prettier/prettier
-(async () => {
-  console.log('Starting Playground...')
-})()
