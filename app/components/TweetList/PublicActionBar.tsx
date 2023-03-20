6eab45eace4e41951a137561df27a8aa7fe475f8
Jake Correa <jcorrea257@gmail.com>
2023-03-20 09:29:24 -0700

    feat: toasts & spinners for tweet rating actions (#19)

--- app/components/TweetList/PublicActionBar.tsx
+++ app/components/TweetList/PublicActionBar.tsx
@@ -1,44 +1,70 @@
+import { useEffect, useState } from 'react'
 import { HandThumbDownIcon, HandThumbUpIcon } from '@heroicons/react/24/outline'
 import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
 import { useFetcher } from '@remix-run/react'
+import { toast } from 'react-hot-toast'
 import { useZorm } from 'react-zorm'
 
+import { useIsSubmitting } from '~/lib/hooks'
 import { buildSendTweetUrl, tw } from '~/lib/utils'
 import { RateTweetSchema } from '~/routes/api+/rate-tweet'
 import type { GeneratedTweet } from '~/services/openai'
 
+const TOAST_ID = 'rate-tweet-toast'
+
 export const PublicActionBar = ({ tweet }: { tweet: GeneratedTweet }) => {
-  const zo = useZorm('rate-tweet', RateTweetSchema)
   const fetcher = useFetcher()
-  const hasSubmitted = Boolean(fetcher.state === 'submitting' || fetcher.data)
+  const [vote, setVote] = useState<'upvote' | 'downvote' | null>(null)
+  const zoUpvote = useZorm('upvote-tweet', RateTweetSchema, { onValidSubmit: () => setVote('upvote') })
+  const zoDownvote = useZorm('downvote-tweet', RateTweetSchema, { onValidSubmit: () => setVote('downvote') })
+  const isSubmitting = useIsSubmitting(fetcher)
+  const isUpvoting = isSubmitting && vote === 'upvote'
+  const isDownvoting = isSubmitting && vote === 'downvote'
+
+  useEffect(() => {
+    if (fetcher.data?.upvoted) {
+      toast.success('Thanks for the feedback!', { id: TOAST_ID })
+      setVote(fetcher.data.upvoted ? 'upvote' : 'downvote')
+    } else if (fetcher.data?.error) {
+      setVote(null)
+    }
+  }, [fetcher.data])
 
   return (
     <div className="flex w-full items-center justify-between gap-4 pt-1 pb-2">
-      <fetcher.Form ref={zo.ref} action="/api/rate-tweet" method="post" className="inline-flex gap-3">
-        <input readOnly name={zo.fields.tweetId()} type="hidden" value={tweet.id} />
-        <button
-          className={tw('text-neutral', hasSubmitted && 'cursor-default', fetcher.data?.upvoted && 'opacity-20')}
-          disabled={hasSubmitted}
-        >
-          <HandThumbDownIcon className="h-6 w-6" />
-          <span className="sr-only">Dislike</span>
-        </button>
+      <div className="inline-flex gap-3">
+        <fetcher.Form ref={zoDownvote.ref} action="/api/rate-tweet" method="post" className="inline">
+          <input readOnly name={zoDownvote.fields.tweetId()} type="hidden" value={tweet.id} />
+          <button
+            className={tw(
+              'btn-ghost btn-sm btn-circle btn !text-neutral disabled:bg-transparent',
+              isDownvoting && 'loading',
+              vote === 'upvote' && 'opacity-30 grayscale'
+            )}
+            disabled={Boolean(vote)}
+          >
+            {!isDownvoting && <HandThumbDownIcon className="h-6 w-6" />}
+            <span className="sr-only">Dislike</span>
+          </button>
+        </fetcher.Form>
 
-        <input readOnly name={zo.fields.tweetId()} type="hidden" value={tweet.id} />
-        <button
-          className={tw(
-            'text-primary',
-            hasSubmitted && 'cursor-default',
-            fetcher.data?.upvoted === false && 'opacity-20'
-          )}
-          disabled={hasSubmitted}
-          name={zo.fields.upvote()}
-          value="upvote"
-        >
-          <HandThumbUpIcon className="h-6 w-6" />
-          <span className="sr-only">Like</span>
-        </button>
-      </fetcher.Form>
+        <fetcher.Form ref={zoUpvote.ref} action="/api/rate-tweet" method="post" className="inline">
+          <input readOnly name={zoDownvote.fields.tweetId()} type="hidden" value={tweet.id} />
+          <button
+            className={tw(
+              'btn-ghost btn-sm btn-circle btn !text-success disabled:bg-transparent',
+              isUpvoting && 'loading',
+              vote === 'downvote' && 'opacity-30 grayscale'
+            )}
+            disabled={Boolean(vote)}
+            name={zoDownvote.fields.upvote()}
+            value="upvote"
+          >
+            {!isUpvoting && <HandThumbUpIcon className="h-6 w-6" />}
+            <span className="sr-only">Like</span>
+          </button>
+        </fetcher.Form>
+      </div>
 
       <a
         target="_blank"
