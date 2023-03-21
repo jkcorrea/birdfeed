69ab5c5684f5141479d2a7afaa14025cf8bea88e
Jake Correa <jcorrea257@gmail.com>
2023-03-21 11:03:41 -0700

    imp: cache data prefetches

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,6 @@
 import { PassThrough } from 'stream'
 
-import type { EntryContext } from '@remix-run/node'
+import type { EntryContext, HandleDataRequestFunction } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import isbot from 'isbot'
@@ -55,4 +55,23 @@ export default function handleRequest(
     )
     setTimeout(abort, ABORT_DELAY)
   })
+}
+
+export const handleDataRequest: HandleDataRequestFunction = async (response, { request }) => {
+  const isGet = request.method.toLowerCase() === 'get'
+  const purpose =
+    request.headers.get('Purpose') ||
+    request.headers.get('X-Purpose') ||
+    request.headers.get('Sec-Purpose') ||
+    request.headers.get('Sec-Fetch-Purpose') ||
+    request.headers.get('Moz-Purpose')
+  const isPrefetch = purpose === 'prefetch'
+
+  // If it's a GET request and it's a prefetch request and it doesn't have a Cache-Control header
+  if (isGet && isPrefetch && !response.headers.has('Cache-Control')) {
+    // we will cache for 5 seconds only on the browser
+    response.headers.set('Cache-Control', 'private, max-age=5')
+  }
+
+  return response
 }
