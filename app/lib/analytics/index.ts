0c28351901db5b76101cf143fe3d80be266f74ee
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-26 15:44:31 -0700

    analytics proxy

--- app/lib/analytics/index.ts
+++ app/lib/analytics/index.ts
@@ -4,7 +4,7 @@ import { posthog } from 'posthog-js'
 export function initAnalytics(loaded?: (posthog: PostHog) => void): void {
   if (process.env.NODE_ENV === 'production') {
     posthog.init('phc_deqi12dJ7yEp5PXhDRncRvDdBJ8v34vGOczaD73UaA9', {
-      api_host: 'https://app.posthog.com',
+      api_host: 'https://dawn-bar-63d4.jahilliard.com',
       autocapture: false,
       capture_performance: false,
       capture_pageview: false,
@@ -12,7 +12,7 @@ export function initAnalytics(loaded?: (posthog: PostHog) => void): void {
       disable_session_recording: true,
       loaded,
       // if needed for debugging
-      debug: true,
+      // debug: true,
     })
     // services to set up on initial show
   }
