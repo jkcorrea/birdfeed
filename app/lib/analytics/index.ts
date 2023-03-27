ecbe18a2e63cf8dc7f1cdd8943c4a91a06c67ef9
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-03-27 09:31:51 -0700

    adding shit

--- app/lib/analytics/index.ts
+++ app/lib/analytics/index.ts
@@ -4,6 +4,7 @@ import { posthog } from 'posthog-js'
 export function initAnalytics(loaded?: (posthog: PostHog) => void): void {
   if (process.env.NODE_ENV === 'production') {
     posthog.init('phc_deqi12dJ7yEp5PXhDRncRvDdBJ8v34vGOczaD73UaA9', {
+      // api_host: 'https://app.posthog.com',
       api_host: 'https://dawn-bar-63d4.jahilliard.com',
       autocapture: false,
       capture_performance: false,
