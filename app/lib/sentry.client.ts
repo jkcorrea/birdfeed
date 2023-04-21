cf6a1602cf22b6bac5df4ae4ffe293ca52a57e2d
Jake Correa <jcorrea257@gmail.com>
2023-04-21 08:54:31 -0700

    fix: enable sentry tracing before init

--- app/lib/sentry.client.ts
+++ app/lib/sentry.client.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useLocation, useMatches } from '@remix-run/react'
 import * as Sentry from '@sentry/remix'
 
+Sentry.addTracingExtensions()
 Sentry.init({
   dsn: 'https://f8dfe328b8794fb69f532de9a09d1d03:706b1268f8e748f18717ee5dde8e978c@o4505052725313536.ingest.sentry.io/4505052727410688',
   tracesSampleRate: 0.01,
@@ -11,4 +12,3 @@ Sentry.init({
     }),
   ],
 })
-Sentry.addTracingExtensions()
